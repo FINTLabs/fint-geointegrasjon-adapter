4cc67dfabef2439124e06e7ceeb2d1b8a9c45314
Asgeir Nilsen <asgeir@fintlabs.no>
2021-02-08 12:44:47 +0100

    Prettier exception message.

--- src/main/java/no/fint/geointegrasjon/service/geointegrasjon/NotFoundException.java
+++ src/main/java/no/fint/geointegrasjon/service/geointegrasjon/NotFoundException.java
@@ -2,6 +2,6 @@ package no.fint.geointegrasjon.service.geointegrasjon;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(Throwable cause) {
-        super(cause);
+        super(cause.getMessage(), cause);
     }
 }
