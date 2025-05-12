0c04d82c29deff32699785ed67dbcaaa42b62943
Nils-Odd Solberg <nilsodd@fintlabs.no>
2025-05-12 13:27:30 +0200

    Adding org to the client header

--- src/main/java/no/fint/adapter/sse/SseInitializer.java
+++ src/main/java/no/fint/adapter/sse/SseInitializer.java
@@ -56,7 +56,7 @@ public class SseInitializer {
                             FintEventListener fintEventListener = new FintEventListener(component, eventHandlerService);
                             fintSse.connect(fintEventListener, ImmutableMap.of(
                                     HeaderConstants.ORG_ID, orgId,
-                                    HeaderConstants.CLIENT, "geointegrasjon-adapter",
+                                    HeaderConstants.CLIENT, "geointegrasjon-adapter@" + orgId,
                                     "x-allowed-asset-ids", orgId
                                     ));
                             sseClients.add(fintSse);
